6e28f4585735a784aace18f8352fd1d2e0aab744
wrathematics <wrathematics@gmail.com>
2017-01-18 10:59:19 -0500

    forgot this one too

---
+++ Rpkg/src/hashtable/sylcount.h
@@ -0,0 +1,44 @@
+#ifndef SYLCOUNT_H_
+#define SYLCOUNT_H_
+
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+#include "hash.h"
+
+typedef struct sylcount sylcount_t;
+
+#define SYLCOUNT_MAX(a,b) (a<b?b:a)
+
+
+static inline bool isvowel(const char c)
+{
+  return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y' ||
+          c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'Y');
+}
+
+static inline int count_syllables(char *str, const int len)
+{
+  int nsyls;
+  const sylcount_t *const restrict s = in_word_set(str, len);
+  
+  if (NULL == s)
+  {
+    nsyls = 0;
+    for (int i=0; i<=len; i++)
+    {
+      if (isvowel(str[i]) && !isvowel(str[i-1]))
+        nsyls++;
+    }
+  }
+  else
+    nsyls = s->syls;
+  
+  // printf("%d %s %d\n", s==NULL, str, nsyls);
+  
+  return SYLCOUNT_MAX(1, nsyls);
+}
+
+
+#endif
