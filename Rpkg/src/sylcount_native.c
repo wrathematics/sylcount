3034616603f5e93716a26f9979c219bc11b88643
wrathematics <wrathematics@gmail.com>
2017-04-04 07:30:26 -0400

    native routines

---
+++ Rpkg/src/sylcount_native.c
@@ -0,0 +1,20 @@
+/* Automatically generated. Do not edit by hand. */
+  
+  #include <R.h>
+  #include <Rinternals.h>
+  #include <R_ext/Rdynload.h>
+  #include <stdlib.h>
+
+extern SEXP R_readability(SEXP s_);
+extern SEXP R_sylcount(SEXP s_, SEXP counts_only_);
+
+static const R_CallMethodDef CallEntries[] = {
+  {"R_readability", (DL_FUNC) &R_readability, 1},
+  {"R_sylcount", (DL_FUNC) &R_sylcount, 2},
+  {NULL, NULL, 0}
+};
+void R_init_sylcount(DllInfo *dll)
+                       {
+                       R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+                       R_useDynamicSymbols(dll, FALSE);
+                       }
