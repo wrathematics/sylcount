6aff7bcdd6c9b27987f20f41695c2b02747ba26d
wrathematics <wrathematics@gmail.com>
2017-04-04 07:38:34 -0400

    add sylcount test

---
+++ Rpkg/tests/sylcount.r
@@ -0,0 +1,27 @@
+library(sylcount)
+
+a = "I am the very model of a modern major general."
+b = "I have information vegetable, animal, and mineral."
+x = c(a, b)
+y = paste0(a, b, collapse=" ")
+
+test = sum(sylcount(a)[[1]])
+truth = 16L
+stopifnot(identical(truth, test))
+
+test = sum(sylcount(b)[[1]])
+truth = 17L
+stopifnot(identical(truth, test))
+
+
+
+test = sylcount(x)
+truth = list(
+  as.integer(c(1, 1, 1, 2, 2, 1, 1, 2, 2, 3)),
+  as.integer(c(1, 1, 4, 4, 3, 1, 3))
+)
+stopifnot(identical(test, truth))
+
+
+test = sylcount(y)[[1]]
+stopifnot(identical(test, unlist(truth)))
