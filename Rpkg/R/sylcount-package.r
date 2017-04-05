7df6e3385c91e32beb6045b8d063b42221698c5b
wrathematics <wrathematics@gmail.com>
2017-04-04 20:53:08 -0400

    tweak package description

--- Rpkg/R/sylcount-package.r
+++ Rpkg/R/sylcount-package.r
@@ -1,6 +1,6 @@
 #' sylcount: Syllable Counting and Readability Measurements
 #' 
-#' TODO
+#' An English language syllable counter, plus readability score measure-er.
 #' 
 #' @useDynLib sylcount R_readability R_sylcount
 #' 
