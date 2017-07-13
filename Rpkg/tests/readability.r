76a6823d3e4da3aabb94ef5244d44faf0aa83a09
wrathematics <wrathematics@gmail.com>
2017-07-12 21:22:54 -0400

    rename

--- Rpkg/tests/readability.r
+++ Rpkg/tests/readability.r
@@ -2,7 +2,7 @@ library(sylcount)
 a <- "I am the very model of a modern major general."
 b <- "I have information vegetable, animal, and mineral."
 
-test = doc_counts(c(a, b))
+test = doc_counts(c(a, b), nthreads=1)
 truth = 
 data.frame(
   chars = c(46L, 50L), 
@@ -15,3 +15,12 @@ data.frame(
 )
 
 stopifnot(identical(test, truth))
+
+
+
+test = readability(paste0(a, b, collapse=" "), nthreads=1)
+stopifnot(all.equal(round(test$re), 34))
+stopifnot(all.equal(round(test$gl), 11))
+stopifnot(all.equal(test$ari, 5L))
+stopifnot(all.equal(round(test$smog), 12))
+stopifnot(all.equal(round(test$cl), 7))
